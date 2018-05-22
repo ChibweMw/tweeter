ed4a0794d8f3452a7a5d356d289bd31fb23d7916
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-05-22 22:04:24 +0000

    counter turns red when numbers are negative

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     var counter = $(this).closest(".container").find('.counter');
     var charLimit = 140;
     var charCount = charLimit - this.value.length;
+    if (charCount < 0) {
+      counter.addClass("red");
+    } else {
+      counter.removeClass("red");
+    }
     counter.text(charCount);
   });
 });
