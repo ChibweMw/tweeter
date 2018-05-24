7d83ef4fa3a8af5ca5ee58a2fc9f1c5a438d787b
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-05-24 05:07:11 +0000

    adding monmgo db

--- server/index.js
+++ server/index.js
@@ -10,8 +10,17 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
+const MongoClient = require("mongodb").MongoClient;
+const MONGO_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGO_URI, (err, db) => {
+  if(err) {
+    console.log(`Failed to connect with ${MONGO_URI}`);
+    throw err;
+  }
+  console.log(`CONNECTED to ${MONGO_URI}`);
+});
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -20,7 +29,7 @@ const db = require("./lib/in-memory-db");
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+const DataHelpers = require("./lib/data-helpers.js")(MongoClient);
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
