a2db22c657bf2e07a57a288f39f1e1e8f421cc71
ChibweMw <chibwemweene@gmail.com>
2024-09-18 16:59:52 -0700

    Catch and log mongoclient startup connection errors.

--- server/index.js
+++ server/index.js
@@ -8,24 +8,35 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const url = "mongodb://localhost:27017/tweeter";
-const client = new MongoClient(url);
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 const dbName = 'tweeter';
 
 
 async function runMongoClient() {
-  await client.connect();
-  console.log('Connected successfully to MongoDB Server')
-  const db = client.db(dbName);
+  try {
+    await client.connect();
+    await client.db(dbName).command({ ping: 1 });
+    console.log('Connected successfully to MongoDB Server')
+    const db = client.db(dbName)
 
-  const DataHelpers = require("./lib/data-helpers.js")(db);
-  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-  app.use('/tweets', tweetsRoutes);
+    const DataHelpers = require("./lib/data-helpers.js")(db);
+    const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+    app.use('/tweets', tweetsRoutes);
 
-  app.listen(PORT, () => {
-    console.log("Example app listening on port " + PORT);
-  });
+    app.listen(PORT, () => {
+      console.log("Example app listening on port " + PORT);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-runMongoClient();
+runMongoClient().catch(console.dir);
